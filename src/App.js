58325061d2e3ba55bb37e99c8585d7e1b0b12249
barbaragagro <barbaragagro1@gmail.com>
2023-01-06 17:53:19 +0100

    added more practice

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import ContextComponent from './ContextComponent';
+import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 
 export  const Context = React.createContext(); 
 
@@ -13,7 +15,10 @@ function App() {
 
   const [name, setname] = useState('')
   const prevName =useRef('')
+
+ const [lastName, setLastName] = useLocalStorage('Name', '')
   
+useUpdateLogger(lastName)
 
  const toggleTheme = () => {
       setdarkTheme((prevDarkTheme)=>!prevDarkTheme)
@@ -38,12 +43,14 @@ function App() {
      
       <h1 className='h1c'>React Hooks Practice Project</h1>
       <br/>
+      {/* <h3>UseContext</h3> */}
       <Context.Provider value={darkTheme}>
      
         <button className='button' onClick={toggleTheme}>Toggle</button>
         <ContextComponent/>
       </Context.Provider>
 <br/>
+{/* <h3>UseRef</h3> */}
       <div className='second'>
         <h3>My name is {name}</h3>
         <h3>My name used to be {prevName.current}</h3>
@@ -56,7 +63,21 @@ function App() {
         <h3>I rendered {renderCount.current} times.</h3>
         <br/>
       </div>
-    
+
+      {/* <h3>CustomHook</h3> */}
+
+      <div className='second'>
+      <h3>Whatever you type in the box below will be saved in Local Storage!</h3>
+        <input 
+        className='inputc'
+        type='text'
+        value={lastName}
+        onChange={e=>setLastName(e.target.value)}
+        />
+     
+      </div>
+      <br/>
+      <br/>
     </div>
   );
 }
